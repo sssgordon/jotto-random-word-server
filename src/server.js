cb7557ca7424b949096dc13a4ff736e7e5e1fff2
sssgordon <gso7@uwo.ca>
2020-01-12 10:11:24 +0100

    Change five letter words json file path

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(
 );
 
 // read words from json file
-const fileContents = fs.readFileSync("./five-letter-words.json", "utf-8");
+const fileContents = fs.readFileSync("../five-letter-words.json", "utf-8");
 const words = JSON.parse(fileContents);
 const { fiveLetterWords } = words;
 
